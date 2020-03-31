8602e42d24f7e3cc9dc4ca25f31bb18751465aa1
João Gabriel Rossi <bielrossiborba@gmail.com>
2020-03-31 17:25:00 -0300

    Update config.py

--- bots/config.py
+++ bots/config.py
@@ -10,8 +10,8 @@ def create_api():
     consumer_secret = os.getenv("CONSUMER_SECRET")
     access_token = os.getenv("ACCESS_TOKEN")
     access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-    auth = tweepy.OAuthHandler("fayA9tzVCd8w2mEsjDegB685U", "OchZUygZm14Du8jgziHCAFs7ZH1NQZI2PZgITkHhwWsPOI8KS4")
-    auth.set_access_token("1244755774854893569-tNk42eHSqWO2Kwt9kgCOmKvFKU6bWZ", "f2vr1PXDtVdpxQQSU4UK6I6tHCGNMQ1SAJmqfdkJ2NPJu")
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
