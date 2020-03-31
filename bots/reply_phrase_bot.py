1f6b2836e92d1201fbb7925cf1877a1d918ec068
bielrossi15 <bielrossiborba@gmail.com>
2020-03-30 23:03:42 -0300

    adding replies and fixing bugs

--- bots/reply_phrase_bot.py
+++ bots/reply_phrase_bot.py
@@ -14,8 +14,8 @@ class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
         line = rand_line()
-        print("answering @" + status.user.screen_name + " with : " + line)
-        self.api.update_status(status="@" + status.user.screen_name + line, in_reply_to_status_id=status.id)   
+        print("answering @" + status.user.screen_name + " with : " + '@' + status.user.screen_name + ' ' + line)
+        self.api.update_status(status='@' + status.user.screen_name + ' ' + line, in_reply_to_status_id=status.id)   
 
     def on_error(self, status):
         logger.error("Error on status", exc_info=True)
@@ -32,7 +32,7 @@ def main():
     api = create_api()
     listener = MyStreamListener(api)
     stream = tweepy.Stream(api.auth, listener)
-    stream.filter(track=["babu"])
+    stream.filter(track=["bora jogar bot", "escolhe pra mim bot"])
 
 if __name__ == "__main__":
     main()
